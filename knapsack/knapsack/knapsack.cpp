e6fdeee8965deda8c787fb3e4ff7ef6ee976eb32
Astrophore <1203033d@student.tp.edu.sg>
2019-05-16 15:14:54 +0800

    added mod functions

--- knapsack/knapsack/knapsack.cpp
+++ knapsack/knapsack/knapsack.cpp
@@ -2,8 +2,37 @@
 //
 
 #include <iostream>
+#include <string>
+#include <stdio.h>
+#include <cmath>
+#include <bitset>
+#include <vector>
+#include <functional>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
 
 using namespace std;
+
+int gcd(int a1, int a2) {
+	int tmp;
+	while (a2 != 0) {
+		tmp = a1;
+		a1 = a2;
+		a2 = tmp % a2;
+	}
+	return a1;
+}
+
+int modInverse(int a, int b)
+{
+	a = a % b;
+	for (int x = 1; x < b; x++)
+	{
+		if ((a*x) % b == 1)
+			return x;
+	}
+}
 
 int main()
 {
